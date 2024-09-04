4196f02e3f4763cbd91998f6107ab5325b8ad151
Md. Tareq Mahmood <tareq.py@gmail.com>
2024-09-04 15:20:55 -0500

    setup and gitignore

---
+++ p1/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/cs544-wisc/f24/main/p1/autograde.py -O autograde.py
+wget https://raw.githubusercontent.com/cs544-wisc/f24/main/tester.py -O tester.py
+wget https://raw.githubusercontent.com/cs544-wisc/f24/main/p1/.gitignore -O .gitignore
